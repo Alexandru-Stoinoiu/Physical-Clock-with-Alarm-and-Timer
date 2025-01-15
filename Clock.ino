8321e90ff842b77ba67242262e1e8778a14a741f
Alexandru-Stoinoiu <stoinoiualexandru@gmail.com>
2025-01-15 23:44:39 +0200

    Add files via upload

--- Clock.ino
+++ Clock.ino
@@ -1,38 +1,38 @@
 #include <Wire.h>
 
-#define SLAVE_ADDRESS 8 
-
-
+#define SLAVE_ADDRESS 8
 
 const int segmentPins[] = {2, 3, 4, 5, 6, 7, 8}; // Pins for segments A-G
 const int digitPins[] = {9, 10, 11, 12}; // Pins for digits 1-4
 const int buzzerPin = 13; // Pin for the buzzer
 
 const int digitPatterns[] = {
-  0b11111100, 
-  0b01100000, 
-  0b11011010, 
-  0b11110010, 
-  0b01100110, 
-  0b10110110, 
-  0b10111110, 
-  0b11100000, 
-  0b11111110, 
-  0b11110110  
+  0b11111100, 0b01100000, 0b11011010, 0b11110010, 0b01100110,
+  0b10110110, 0b10111110, 0b11100000, 0b11111110, 0b11110110
 };
 
 int hours = 12;
 int minutes = 0;
-int alarmHours = 12;
-int alarmMinutes = 1; // Set alarm time here
+int alarmHours = 0;
+int alarmMinutes = 0;
 unsigned long lastUpdateTime = 0;
 bool alarmTriggered = false;
 bool alarmActive = false;
 unsigned long alarmStartTime = 0;
+int mode = 0; // 0 - Clock, 1 - Timer
+
+int realHours = 12;
+int realMinutes = 0;
+unsigned long lastRealTimeUpdate = 0;
+
+int timerHours = 0;
+int timerMinutes = 0;
+unsigned long timerStartTime = 0;
+bool timerRunning = false;
 
 void setup() {
-  Wire.begin(SLAVE_ADDRESS); // Initialize I2C as slave
-  Wire.onReceive(receiveEvent); // Register receive event
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onReceive(receiveEvent);
   for (int i = 0; i < 7; i++) {
     pinMode(segmentPins[i], OUTPUT);
   }
@@ -40,30 +40,26 @@ void setup() {
     pinMode(digitPins[i], OUTPUT);
   }
   pinMode(buzzerPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   unsigned long currentTime = millis();
   
-  if (currentTime - lastUpdateTime >= 60000) { // Check every minute
-    updateTime();
-    lastUpdateTime = currentTime;
+  if (currentTime - lastRealTimeUpdate >= 60000) {
+    updateRealTime();
+    lastRealTimeUpdate = currentTime;
   }
   
-  displayTime();
-  
-  if (alarmTriggered) {
-    if (!alarmActive) {
-      triggerAlarm();
-      alarmActive = true;
-      alarmStartTime = currentTime;
-    }
-    if (currentTime - alarmStartTime >= 5000) { // Stop alarm after 5 seconds
-      noTone(buzzerPin);
-      alarmTriggered = false;
-      alarmActive = false;
-    }
+  if (mode == 0) {
+    hours = realHours;
+    minutes = realMinutes;
+    displayTime();
+  } else if (mode == 1) {
+    updateAndDisplayTimer();
   }
+  
+  checkAlarm();
 }
 
 void displayTime() {
@@ -84,32 +80,92 @@ void displayTime() {
   }
 }
 
-void updateTime() {
-  minutes++;
-  if (minutes == 60) {
-    minutes = 0;
-    hours++;
-    if (hours == 24) {
-      hours = 0;
-    }
+void updateRealTime() {
+  realMinutes++;
+  if (realMinutes == 60) {
+    realMinutes = 0;
+    realHours = (realHours + 1) % 24;
   }
   
-  if (hours == alarmHours && minutes == alarmMinutes) {
+  if (realHours == alarmHours && realMinutes == alarmMinutes) {
     alarmTriggered = true;
   }
 }
 
-void triggerAlarm() {
-  tone(buzzerPin, 1000, 500); // 1000 Hz tone for 500 ms
-  delay(500);
-  tone(buzzerPin, 1000, 500); // 1000 Hz tone for 500 ms
-  delay(500);
-  tone(buzzerPin, 1000, 500); // 1000 Hz tone for 500 ms
+void updateAndDisplayTimer() {
+  if (timerRunning) {
+    unsigned long elapsedSeconds = (millis() - timerStartTime) / 1000;
+    int remainingSeconds = (timerHours * 3600 + timerMinutes * 60) - elapsedSeconds;
+    
+    if (remainingSeconds <= 0) {
+      timerRunning = false;
+      alarmTriggered = true;
+      mode = 0; // Switch back to clock mode
+    } else {
+      hours = remainingSeconds / 3600;
+      minutes = (remainingSeconds % 3600) / 60;
+      displayTime();
+    }
+  } else {
+    hours = timerHours;
+    minutes = timerMinutes;
+    displayTime();
+  }
+}
+
+void checkAlarm() {
+  if (alarmTriggered) {
+    if (!alarmActive) {
+      alarmActive = true;
+    }
+    tone(buzzerPin, 1000); // Continuous tone
+  }
+}
+
+
+void stopAlarm() {
+  alarmTriggered = false;
+  alarmActive = false;
+  noTone(buzzerPin);
 }
 
 void receiveEvent(int howMany) {
-  while (Wire.available()) {
-    char c = Wire.read(); // Receive byte as a character
-    Serial.write(c);
+  if (Wire.available()) {
+    char command[6];
+    int i = 0;
+    while (Wire.available() && i < 5) {
+      command[i++] = Wire.read();
+    }
+    command[i] = '\0';
+    
+    if (strcmp(command, "Clock") == 0) {
+      mode = 0;
+      hours = realHours;
+      minutes = realMinutes;
+    } else if (strcmp(command, "Alarm") == 0) {
+      mode = 0;
+      if (Wire.available() >= 2) {
+        alarmHours = Wire.read();
+        alarmMinutes = Wire.read();
+        Serial.println("Alarm set to:");
+        Serial.print(alarmHours);
+        Serial.print(" : ");
+        Serial.print(alarmMinutes);
+      }
+    } else if (strcmp(command, "Timer") == 0) {
+      mode = 1;
+      if (Wire.available() >= 2) {
+        timerHours = Wire.read();
+        timerMinutes = Wire.read();
+        timerStartTime = millis();
+        timerRunning = true;
+        Serial.println("Timer set to:");
+        Serial.print(timerHours);
+        Serial.print(" : ");
+        Serial.print(timerMinutes);
+      }
+    } else if (strcmp(command, "StopA") == 0) {
+      stopAlarm();
+    }
   }
 }
--- master.ino
+++ master.ino
@@ -1,6 +1,8 @@
 #include <LiquidCrystal.h>
 #include <Wire.h>
 
+#define MASTER_ADDRESS 3
+
 // LCD pin configuration
 const int rs = 8, en = 3, d4 = 7, d5 = 6, d6 = 5, d7 = 4;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -11,27 +13,28 @@ const int JOY_Y = A1;
 const int JOY_SW = 2;
 
 // Menu items
-const int NUM_ITEMS = 3;
-String menuItems[NUM_ITEMS] = {"Clock", "Timer", "Alarm"};
+const int NUM_ITEMS = 4;
+String menuItems[NUM_ITEMS] = {"Clock", "Timer", "Alarm", "StopAlarm"};
 
 int currentItem = 0;
 int lastJoyY = 512;
 bool joyPressed = false;
+bool alarmRinging = false;
 
 void setup() {
-  Wire.begin();
+  Wire.begin(MASTER_ADDRESS);
   lcd.begin(16, 2);
   pinMode(JOY_SW, INPUT_PULLUP);
   updateMenu();
+  Serial.begin(9600);
 }
 
 void loop() {
-  Wire.beginTransmission(8);
-  Wire.write("Test");
-  Wire.endTransmission();
-  navigateMenu();
+  if (!alarmRinging) {
+    navigateMenu();
+  }
   checkSelection();
-  Wire.endTransmission();
+  handleAlarmRinging();
 }
 
 void updateMenu() {
@@ -59,13 +62,179 @@ void navigateMenu() {
 void checkSelection() {
   if (digitalRead(JOY_SW) == LOW && !joyPressed) {
     joyPressed = true;
-    lcd.clear();
-    lcd.print("Selected:");
-    lcd.setCursor(0, 1);
-    lcd.print(menuItems[currentItem]);
-    delay(1500);
-    updateMenu();
+    if (alarmRinging) {
+      stopAlarm();
+    } else {
+      lcd.clear();
+      lcd.print("Selected:");
+      lcd.setCursor(0, 1);
+      lcd.print(menuItems[currentItem]);
+      if (menuItems[currentItem] == "Clock") {
+        Wire.beginTransmission(8);
+        Wire.write("Clock");
+        Wire.endTransmission();
+      } else if (menuItems[currentItem] == "Alarm") {
+        setAlarm();
+      } else if (menuItems[currentItem] == "Timer") {
+        setTimer();
+      } else if (menuItems[currentItem] == "StopAlarm"){
+        Serial.println("STOOOP");
+        Wire.beginTransmission(8);
+        Wire.write("StopA");
+        Wire.endTransmission();
+      }
+      delay(1500);
+      updateMenu();
+    }
   } else if (digitalRead(JOY_SW) == HIGH) {
     joyPressed = false;
   }
+}
+
+void stopAlarm() {
+  Wire.beginTransmission(8);
+  Wire.write("StopAlarm");
+  Wire.endTransmission();
+  alarmRinging = false;
+  updateMenu();
+}
+
+void handleAlarmRinging() {
+  if (alarmRinging) {
+    lcd.clear();
+    lcd.print("Alarm is ringing");
+    lcd.setCursor(0, 1);
+    lcd.print("Press to stop");
+  }
+}
+
+void setAlarm() {
+  int alarmHour = 0;
+  int alarmMinute = 0;
+  setTime("Alarm", alarmHour, alarmMinute);
+  
+  Wire.beginTransmission(8);
+  Wire.write("Alarm");
+  Wire.write(alarmHour);
+  Wire.write(alarmMinute);
+  Wire.endTransmission();
+  
+  displaySetTime("Alarm set to:", alarmHour, alarmMinute);
+}
+
+void setTimer() {
+  int timerHour = 0;
+  int timerMinute = 0;
+  setTime("Timer", timerHour, timerMinute);
+  
+  Wire.beginTransmission(8);
+  Wire.write("Timer");
+  Wire.write(timerHour);
+  Wire.write(timerMinute);
+  Wire.endTransmission();
+  
+  displaySetTime("Timer set to:", timerHour, timerMinute);
+}
+
+void setTime(const char* mode, int &hours, int &minutes) {
+  int lastJoyY = analogRead(JOY_Y);
+  unsigned long lastDebounceTime = 0;
+  unsigned long debounceDelay = 50;
+  bool joyMoved = false;
+  bool buttonPressed = false;
+
+  delay(500);
+
+  // Set hour
+  while (true) {
+    lcd.clear();
+    lcd.print("Set ");
+    lcd.print(mode);
+    lcd.print(" Hour:");
+    lcd.setCursor(0, 1);
+    lcd.print(hours);
+    
+    int joyY = analogRead(JOY_Y);
+    if (abs(joyY - lastJoyY) > 50) {
+      lastDebounceTime = millis();
+    }
+
+    if ((millis() - lastDebounceTime) > debounceDelay) {
+      if (joyY < 300 && !joyMoved) {
+        hours = (hours - 1 + 24) % 24;
+        joyMoved = true;
+      } else if (joyY > 700 && !joyMoved) {
+        hours = (hours + 1) % 24;
+        joyMoved = true;
+      } else if (joyY >= 300 && joyY <= 700) {
+        joyMoved = false;
+      }
+    }
+
+    lastJoyY = joyY;
+    
+    if (digitalRead(JOY_SW) == LOW && !buttonPressed) {
+      buttonPressed = true;
+      delay(50);
+    } else if (digitalRead(JOY_SW) == HIGH && buttonPressed) {
+      buttonPressed = false;
+      break;
+    }
+    
+    delay(50);
+  }
+  
+  // Set minute (similar structure as hour setting)
+  lastJoyY = analogRead(JOY_Y);
+  joyMoved = false;
+  buttonPressed = false;
+  
+  while (true) {
+    lcd.clear();
+    lcd.print("Set ");
+    lcd.print(mode);
+    lcd.print(" Minute:");
+    lcd.setCursor(0, 1);
+    lcd.print(minutes);
+    
+    int joyY = analogRead(JOY_Y);
+    if (abs(joyY - lastJoyY) > 50) {
+      lastDebounceTime = millis();
+    }
+
+    if ((millis() - lastDebounceTime) > debounceDelay) {
+      if (joyY < 300 && !joyMoved) {
+        minutes = (minutes - 1 + 60) % 60;
+        joyMoved = true;
+      } else if (joyY > 700 && !joyMoved) {
+        minutes = (minutes + 1) % 60;
+        joyMoved = true;
+      } else if (joyY >= 300 && joyY <= 700) {
+        joyMoved = false;
+      }
+    }
+
+    lastJoyY = joyY;
+    
+    if (digitalRead(JOY_SW) == LOW && !buttonPressed) {
+      buttonPressed = true;
+      delay(50);
+    } else if (digitalRead(JOY_SW) == HIGH && buttonPressed) {
+      buttonPressed = false;
+      break;
+    }
+    
+    delay(50);
+  }
+}
+
+void displaySetTime(const char* message, int hours, int minutes) {
+  lcd.clear();
+  lcd.print(message);
+  lcd.setCursor(0, 1);
+  lcd.print(hours);
+  lcd.print(":");
+  if (minutes < 10) lcd.print("0");
+  lcd.print(minutes);
+  delay(2000);
 }
